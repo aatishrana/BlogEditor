67b57866717cd0277290bb3279ae266f49c30de2
Aatish <aatishrana8@gmail.com>
2017-09-09 13:09:24 +0530

    basic preview added

--- src/app/preview/preview.component.ts
+++ src/app/preview/preview.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Subscription} from 'rxjs/Subscription';
+import {CommonService} from '../common/common.service';
 
 @Component({
   selector: 'app-preview',
   templateUrl: './preview.component.html',
   styleUrls: ['./preview.component.css']
 })
-export class PreviewComponent implements OnInit {
+export class PreviewComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  public unit;
+  public data: any[];
+  private subscriber: Subscription;
+
+  constructor(private service: CommonService) {
+    this.unit = this.service.unit;
+  }
 
   ngOnInit() {
+    this.data = this.service.data;
+    this.subscriber = this.service.dataChanged
+      .subscribe(
+        (data) => {
+          this.data = data;
+        }
+      );
+  }
+
+  ngOnDestroy(): void {
+    if (this.subscriber) {
+      this.subscriber.unsubscribe();
+    }
   }
 
 }
